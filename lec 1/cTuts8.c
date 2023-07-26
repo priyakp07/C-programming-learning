b058be2fcffa3e60a285bd440609fa5efba59477
priyakp07 <priyakp080@gmail.com>
2023-07-26 23:51:31 +0530

    remove all file

---
+++ lec 1/cTuts8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   int a;
+   float b;
+   a = 21;
+   b = 3.4;
+   printf("a + b = %f\n" , a + b);
+   printf("a - b = %f\n" , a - b);
+   printf("a * b = %f\n" , a * b);
+   printf("a / b = %f\n" , a / b);
+    return 0;
+}
