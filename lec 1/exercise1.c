751b33e2ce480f279caa395d9f788ee10bb1171d
priyakp07 <priyakp080@gmail.com>
2023-07-27 22:11:14 +0530

    c language code

---
+++ lec 1/exercise1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+print multiplication table of a number entered by in user in pretty form
+
+example:
+
+input
+enter the number you want multiplication table of:
+6
+
+output table of 6:
+6*1=6
+6*2=12
+6*3=18
+6*4=24
+6*5=30
+6*6=36
+6*7=42
+6*8=48
+6*9=54
+6*10=60
+
+*/
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
