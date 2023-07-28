6858e83ce3015a1d5c91de4155e86517fb38863a
priyakp07 <priyakp080@gmail.com>
2023-07-28 21:42:53 +0530

    remove all .exe file

---
+++ lec 1/lec 2/tutorial1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI = 3.14
+
+int main(int argc, char const *argv[])
+{
+    int a=8;
+    const float b=7.333;
+    printf ("%f", pi);
+    return 0;
+}
---
+++ lec 1/lec 2/tutorial2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main(int argc, char const *argv[])
+{
+    int a = 6;
+ const float b = 7.333;
+    printf("tab character \t\t my backslace \a %f",PI);
+    return 0;
+}
