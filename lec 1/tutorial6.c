4b42ce18baba422eb91a2b8568db38115e0903ef
priyakp07 <priyakp080@gmail.com>
2023-07-30 19:03:29 +0530

    romove .exe file

---
+++ lec 1/tutorial6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i , j;
+    for (i = 0; j < 3 , i < 5 ; i++ , j++)
+    {
+        printf("%d %d\n" , i,j);
+         
+    }
+    return 0;
+}
