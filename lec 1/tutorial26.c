ca74fe66ec1a31a0410161c8e6beb47cbcefea40
priyakp07 <priyakp080@gmail.com>
2023-08-07 22:21:42 +0530

    c exercise

---
+++ lec 1/tutorial26.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i, j, x, k=0;
+    for ( i = 1; i <= 9; i++)
+    {
+          i<6?k++:k--;
+          x=1;
+          {
+       for ( j = 1; j <= 5; j++)
+       
+        if(j>=6-k)
+        {
+        printf("%d" , x);
+         x++;
+       }
+        else
+        printf(" ");
+       
+       }
+       printf("\n");
+    
+}
+    
+    return 0;
+}
+ 
