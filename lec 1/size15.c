775eaf913c953d9409c139923f5f39a6b6840dea
priyakp07 <priyakp080@gmail.com>
2023-08-20 20:29:20 +0530

    arrays exercise

--- lec 1/size15.c
+++ lec 1/size15.c
@@ -21,19 +21,6 @@ int main(int argc, char const *argv[])
        sum[0][2] = A[0][0] + A[0][1];
        sum[1][2] = A[1][0] + A[1][1];
         printf("%d" , sum[i][j]);
-
-    // printf("\nThe sum of colum matrix\n");
-    // for ( i = 0; i <= 1; i++)
-    // {
-    //     printf("\n");
-    //     for(j = 0; j <= 1; j++)
-    //     {
-    //    printf("%d " , A[i][j]);
-    //     }
-    // sum of colums
-    //    sum[2][0] = A[0][0] + A[1][0];
-    //    sum[2][1] = A[0][1] + A[1][1];
-    //     printf("%d" , sum[i][j]);
     }
     return 0;
 }
